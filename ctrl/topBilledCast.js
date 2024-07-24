0da33ae4c371a381e81163e6f7bd10cc53c90c61
Zoex2304 <zikrifadhiilahahmad@gmail.com>
2024-07-25 00:49:14 +0700

    fix again

--- ctrl/topBilledCast.js
+++ ctrl/topBilledCast.js
@@ -1,38 +1,38 @@
 function getBilledCast(movie_id) {
-  const url = `https://api.themoviedb.org/3/movie/${movie_id}/credits`;
-  const data = {
-    language: "en-US",
-    api_key: apiKey,
-  };
+    const url = `https://api.themoviedb.org/3/movie/${movie_id}/credits`;
+    const data = {
+        language: "en-US",
+        api_key: apiKey,
+    };
 
-  $.getJSON(url, data, (response) => {
-    console.log(response)
-    response.cast.forEach(data => 
-        appendBilledCast(data)
-    )
-  }).fail((jqXHR, statusText, errorThrown) => {
-    const errorHeader = errorThrown || "error bang";
-    console.error(errorHeader, statusText, jqXHR.responseText);
-  });
+    $.getJSON(url, data, (response) => {
+        response.cast.forEach(data => appendBilledCast(data));
+    }).fail((jqXHR, statusText, errorThrown) => {
+        const errorHeader = errorThrown || "error bang";
+        console.error(errorHeader, statusText, jqXHR.responseText);
+    });
 }
 
 function appendBilledCast(data) {
-  var photoCast = `<img src='https://image.tmdb.org/t/p/w200/${data.profile_path}'`;
-  var nameCast = data.name;
-  var charraccterCast = data.character;
-  var card = `<div id='cast-${data.id} class='col-lg-2 col-md-3'>
-        <div class='card billed-card billed-img-fluid rounded'>
-            ${photoCast}
-        </div>
-    </div>`;
-  $("#topBilledCast").append(card);
+    const photoCast = data.profile_path ? `<img src='https://image.tmdb.org/t/p/w200/${data.profile_path}' alt='${data.name}'>` : '';
+    var card = `<div id='movie-${data.id}' class='scroll-item col-lg-2 col-md-4 col-sm-4 col-4 mb-4' style='height: fit-content'>
+                        <div class='billed movie-card card h-100'>
+                            ${photoCast}
+                            <div class='card-body ps-0'>
+                                <p> ${data.name} <br>
+                                ${data.character}</p>
+                            </div>
+                        </div>
+                      
+                    </div>`;
+    $("#topBilledCast").append(card);
 }
 
 $(() => {
-  const urlLocal = new URLSearchParams(window.location.search);
-  const id = urlLocal.get("id");
+    const urlLocal = new URLSearchParams(window.location.search);
+    const id = urlLocal.get("id");
 
-  if (id) {
-    getBilledCast(id);
-  }
+    if (id) {
+        getBilledCast(id);
+    }
 });
